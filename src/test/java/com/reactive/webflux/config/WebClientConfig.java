3f0a1ca6f252eed41402f66a6a1f6192ff338daa
prashant09.thakur <prashant09.thakur@gmail.com>
2021-11-14 12:50:17 +0530

    generating token

--- src/test/java/com/reactive/webflux/config/WebClientConfig.java
+++ src/test/java/com/reactive/webflux/config/WebClientConfig.java
@@ -2,7 +2,12 @@ package com.reactive.webflux.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
@@ -12,7 +17,16 @@ public class WebClientConfig {
 		//we will send response to service built earlier
 		return WebClient.builder()
 				.baseUrl("http://localhost:8080")
+				//.filter((clientRequest,exchangeFunction)->sessionToken(clientRequest,exchangeFunction))
+				//above commented line is same as written below by method reference
+				.filter(this::sessionToken)
+			//	.defaultHeaders(h->h.setBasicAuth("usename","password"))
 				.build();
+	}
+	private Mono<ClientResponse>sessionToken(ClientRequest request, ExchangeFunction ex){
+		System.out.println("generating session token");
+		ClientRequest clientRequest = ClientRequest.from(request).headers(h->h.setBearerAuth("some lengthy - jwt web token")).build();
+		return ex.exchange(clientRequest);
 	}
 	
 }
